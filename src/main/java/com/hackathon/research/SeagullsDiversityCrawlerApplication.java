2b79f308f6ced2bfab3f47c81e74646e5ac87196
mahesh josyula <rammahesh456@gmail.com>
2022-05-29 01:31:52 +0530

    initia ping commit

---
+++ src/main/java/com/hackathon/research/SeagullsDiversityCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeagullsDiversityCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeagullsDiversityCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hackathon/research/api/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.hackathon.research.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/api/ping")
+    public String ping(){
+        return "hello crawler";
+    }
+}
